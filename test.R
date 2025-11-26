1bd9d642338e01e77f36f4a181eed3e194e75aa4
Guofa-Su <73263097+Guofa-Su@users.noreply.github.com>
2025-11-26 10:55:32 +0800

    coding

--- test.R
+++ test.R
@@ -1,5 +1,9 @@
 ##test code
 library(invacost)
+data("invacost")
 summary(invacost)
 system('git pull origin main')
 system('git push origin main')
+data("iris")
+hist(iris$Sepal.Length)
+write.csv("Beij_China/iris.csv")
